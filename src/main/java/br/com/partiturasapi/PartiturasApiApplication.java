2f7f2351feddef1e228035f87bdfb7a3dc3bd658
rafael <rafdgar@gmail.com>
2026-06-03 19:25:48 -0300

    refactor: padroniza package base da aplicação
    - move a aplicação para o package base br.com.partiturasapi
    
    - ajusta pom e propriedades iniciais da base Spring Boot

---
+++ src/main/java/br/com/partiturasapi/PartiturasApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.partiturasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartiturasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartiturasApiApplication.class, args);
+    }
+}
---
+++ src/main/java/br/com/partiturasapi/shared/config/ApplicationClockConfig.java
@@ -0,0 +1,14 @@
+package br.com.partiturasapi.shared.config;
+
+import java.time.Clock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationClockConfig {
+
+    @Bean
+    public Clock systemClock() {
+        return Clock.systemUTC();
+    }
+}
